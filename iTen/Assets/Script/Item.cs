de853879886dc80f3740b98d8c4f49654b57d28e
zxcv0028 <bad020906@gmail.com>
2024-11-04 02:56:26 +0900

    인벤토리 초안

---
+++ iTen/Assets/Script/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item : MonoBehaviour
+{
+    public string itemName;
+    public Sprite icon;
+    public int Amount = 1;
+}
