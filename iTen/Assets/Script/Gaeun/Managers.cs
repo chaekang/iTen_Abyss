4eff3316c69f4b42576aa8e9a7dc69cc77cd7f29
Gaeun <qltenwkd@naver.com>
2024-12-31 00:03:37 +0900

    아트 폴더 정리

---
+++ iTen/Assets/Script/Gaeun/Managers.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitManager()
+    {
+        // GameManager()
+        // SceneExtensionManager()
+        // MonsterManager()
+    }
+}
