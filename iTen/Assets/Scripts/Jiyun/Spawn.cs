93c5368683870976e426700fdb72d5fa380711b1
chyoura <wof526@sookmyung.ac.kr>
2025-01-04 17:59:09 +0900

    1/4 making Room
    룸 매치메이킹 관련 오류 수정,
    이동동기화 진행중 - 클라이언트 문제 해결 요함

--- iTen/Assets/Scripts/Jiyun/Spawn.cs
+++ iTen/Assets/Scripts/Jiyun/Spawn.cs
@@ -7,26 +7,28 @@ using Photon.Realtime;
 public class Spawn : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
+    public GameObject player;
     void Start()
     {
-        PhotonNetwork.SerializationRate = 30;
-        PhotonNetwork.SendRate = 30;
+        //PhotonNetwork.SerializationRate = 30;
+        //PhotonNetwork.SendRate = 30;
         
         SpawnPlayer();
     }
 
     void SpawnPlayer()
     {
-        int spawnIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
-        Transform spawnPoint = SpawnManager.Instance.GetSpawnPoint(spawnIndex);
+        //int spawnIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
+        Transform spawnPoint = SpawnManager.Instance.GetSpawnPoint(0);
 
         if (spawnPoint != null)
         {
-            GameObject player = PhotonNetwork.Instantiate("player", spawnPoint.position, spawnPoint.rotation, 0);
+            //GameObject player = PhotonNetwork.Instantiate("player", spawnPoint.position, spawnPoint.rotation, 0);
+            Instantiate(player, spawnPoint.position, spawnPoint.rotation);
         }
     }    
 
-    public override void OnPlayerEnteredRoom(Player newPlayer)
+    /*public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         base.OnPlayerEnteredRoom(newPlayer);
 
@@ -41,4 +43,5 @@ public class Spawn : MonoBehaviourPunCallbacks
     {
         SpawnPlayer();
     }
+    */
 }
